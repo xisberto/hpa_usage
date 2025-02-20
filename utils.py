01616a0998872bc72bfe65b586681c1090d19441
Humberto Fraga <humbertofraga@gmail.com>
2025-02-20 17:18:55 -0300

    Permite salvar as pesquisas realizadas como CSV

---
+++ utils.py
@@ -0,0 +1,32 @@
+import os
+import glob
+from datetime import datetime
+
+import pandas as pd
+
+
+partial_results = 'partial_results'
+
+
+def save_results(results):
+  search_timestamp = datetime.now().replace(microsecond=0).isoformat()
+
+  if not os.path.isdir(partial_results):
+    os.mkdir(partial_results)
+
+  items.to_csv(f"{partial_results}/{search_timestamp}-phase1.csv", index=False)
+
+
+def load_results():
+  phase1_files = glob.glob(f"{partial_results}/*-phase1.csv")
+
+  items = None
+
+  for file in phase1_files:
+    if items is None:
+      items = pd.read_csv(file)
+    else:
+      df = pd.read_csv(file)
+      items = pd.concat([items, df])
+
+  return items.drop_duplicates()
