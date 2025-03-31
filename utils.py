6a0ae256ddf60a7190e9722249b177c90d2e0c86
Humberto Fraga <humbertofraga@gmail.com>
2025-03-31 12:09:27 -0300

    Permite carregar fase3

--- utils.py
+++ utils.py
@@ -9,30 +9,37 @@ partial_results = 'partial_results'
 
 
 def save_results(results):
-  search_timestamp = datetime.now().replace(microsecond=0).isoformat()
+    search_timestamp = datetime.now().replace(microsecond=0).isoformat()
 
-  if not os.path.isdir(partial_results):
-    os.mkdir(partial_results)
+    if not os.path.isdir(partial_results):
+        os.mkdir(partial_results)
 
-  results.to_csv(f"{partial_results}/{search_timestamp}-phase1.csv", index=False)
+    results.to_csv(
+        f"{partial_results}/{search_timestamp}-phase1.csv", index=False)
 
 
 def load_results():
-  phase1_files = glob.glob(f"{partial_results}/*-phase1.csv")
+    phase1_files = glob.glob(f"{partial_results}/*-phase1.csv")
 
-  items = None
+    items = None
 
-  for file in phase1_files:
-    if items is None:
-      items = pd.read_csv(file)
-    else:
-      df = pd.read_csv(file)
-      items = pd.concat([items, df])
+    for file in phase1_files:
+        if items is None:
+            items = pd.read_csv(file)
+        else:
+            df = pd.read_csv(file)
+            items = pd.concat([items, df])
 
-  return items.drop_duplicates().sort_values('repo_name', ignore_index=True)
+    return items.drop_duplicates().sort_values('repo_name', ignore_index=True)
 
 
 def load_phase2_results():
-  phase2_files = glob.glob(f"{partial_results}/*phase2.csv")
-  phase2_files.sort()
-  return pd.read_csv(phase2_files[-1])
+    phase2_files = glob.glob(f"{partial_results}/*phase2.csv")
+    phase2_files.sort()
+    return pd.read_csv(phase2_files[-1])
+
+
+def load_phase3_results():
+    phase3_files = glob.glob(f"{partial_results}/*phase3.csv")
+    phase3_files.sort()
+    return pd.read_csv(phase3_files[-1])
